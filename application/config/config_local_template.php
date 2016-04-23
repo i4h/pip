40011003844ef893e19bfe93894257fc409d77a2
Ingmar Vierhaus <vierhaus@zib.de>
2016-04-23 19:03:44 +0200

    Created file for server-dependent configuration

---
+++ application/config/config_local_template.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['base_url'] = 'http:://localhost/binary_weather/'; // Base URL including trailing slash (e.g. http://localhost/)
+
+$config['db_host'] = ''; // Database host (e.g. localhost)
+$config['db_name'] = ''; // Database name
+$config['db_username'] = ''; // Database username
+$config['db_password'] = ''; // Database password
