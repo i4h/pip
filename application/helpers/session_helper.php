373a934c077c6377ea7d1c0dc08e1bea1228103b
Ingmar Vierhaus <vierhaus@zib.de>
2016-04-23 20:28:39 +0200

    sessionhelper saves everything using an application key

--- application/helpers/session_helper.php
+++ application/helpers/session_helper.php
@@ -2,16 +2,23 @@
 
 class Session_helper {
 
+	const KEY = 'applicationkey_xyz';
+
 	function set($key, $val)
 	{
-		$_SESSION["$key"] = $val;
+		if (!isset($_SESSION[self::KEY]))
+			$_SESSION[self::KEY] = [];
+		$_SESSION[self::KEY]["$key"] = $val;
 	}
-	
-	function get($key)
+
+	function get($key, $default = null)
 	{
-		return $_SESSION["$key"];
+		if (!isset($_SESSION[self::KEY]))
+			$_SESSION[self::KEY] = [];
+
+		return isset($_SESSION[self::KEY]["$key"]) ? $_SESSION[self::KEY]["$key"] : $default;
 	}
-	
+
 	function destroy()
 	{
 		session_destroy();
